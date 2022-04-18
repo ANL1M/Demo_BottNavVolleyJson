dd9846015a1a92121988b77fcead8eab59ad2a2c
Андрей Лименько <anlimenko@live.ru>
2022-04-18 23:17:38 +0200

    Extracted strings and customized colors

--- app/src/main/java/it/anlim/demobottnavvolleyjson/MainActivity.java
+++ app/src/main/java/it/anlim/demobottnavvolleyjson/MainActivity.java
@@ -1,8 +1,12 @@
 package it.anlim.demobottnavvolleyjson;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialization BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.btmnavMain);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment, R.id.detailsFragment, R.id.settingsFragment).build();
+        NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
