ed983acabd1eec09ec3da60104e9ef6b2db39c66
Андрей Лименько <anlimenko@live.ru>
2022-04-21 00:15:20 +0200

    Added links to my profiles

--- app/src/main/java/it/anlim/demobottnavvolleyjson/SettingsFragment.java
+++ app/src/main/java/it/anlim/demobottnavvolleyjson/SettingsFragment.java
@@ -8,12 +8,12 @@ import android.view.ViewGroup;
 
 public class SettingsFragment extends Fragment {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
